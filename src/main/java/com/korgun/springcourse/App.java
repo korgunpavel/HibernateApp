84044b2b655b834a81341ba578340d34570d3929
Pavel Korgun <korgunpaul@gmail.com>
2024-08-01 14:22:14 +0500

    ManyToMany Actor and Movie

--- src/main/java/com/korgun/springcourse/App.java
+++ src/main/java/com/korgun/springcourse/App.java
@@ -1,6 +1,8 @@
 package com.korgun.springcourse;
 
+import com.korgun.springcourse.model.Actor;
 import com.korgun.springcourse.model.Item;
+import com.korgun.springcourse.model.Movie;
 import com.korgun.springcourse.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -9,18 +11,24 @@ import org.hibernate.cfg.Configuration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class App 
 {
     public static void main( String[] args ) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Actor.class).addAnnotatedClass(Movie.class);
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
             Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
+            Actor actor = session.get(Actor.class,2);
+            System.out.println(actor.getMovies());
 
+            Movie movieRemove = actor.getMovies().get(0);
 
+            actor.getMovies().remove(movieRemove);
+            movieRemove.getActors().remove(actor);
 
 
 
